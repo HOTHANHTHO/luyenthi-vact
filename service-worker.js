6d6ca409fd4bc50a27c971315c6fe62a523c0f99
HOTHANHTHO <31532052+HOTHANHTHO@users.noreply.github.com>
2026-06-19 10:08:59 +0700

    Bump cache 20260619e

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Service worker — cho phép chạy offline sau lần mở đầu (khi đã host qua http/https) */
-var CACHE = "vact-cache-20260619d";
+var CACHE = "vact-cache-20260619e";
 var ASSETS = [
   "./",
   "./index.html",
